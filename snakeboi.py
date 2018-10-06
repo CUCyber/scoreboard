01468b5f6c6040f07b7e54f1bf08f902933d710a
Foster McLane <fkmclane@gmail.com>
2018-10-06 10:25:23 -0400

    add header to html page

--- snakeboi.py
+++ snakeboi.py
@@ -41,7 +41,7 @@ services = {
     'FTP': {'offset': 5, 'port': 21, 'file': 'DONOTDELETE', 'contents': 'asdf', 'dne': 'DOESNOTEXIST'},
     'SSH': {'offset': 6, 'port': 22, 'username': 'asdf', 'password': 'asdf'},
     'HTTP': {'offset': 7, 'port': 80, 'regex': 'asdf'},
-    'MySQL': {'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf'},
+    'MySQL': {'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf', 'db': 'asdf'},
 }
 
 interval = 60
