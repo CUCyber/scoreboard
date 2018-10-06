b00c73b23ac769edf65e62af190937b70d0f2748
Foster McLane <fkmclane@gmail.com>
2018-10-06 15:33:47 -0400

    make pings faster

--- snakeboi/ping.py
+++ snakeboi/ping.py
@@ -5,7 +5,7 @@ def check(addr, **kwargs):
     up = False
 
     try:
-        up = subprocess.call(['ping', '-c4', addr], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
+        up = subprocess.call(['ping', '-i0.1', '-c4', addr], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
     except subprocess.SubprocessError:
         up = False
 
