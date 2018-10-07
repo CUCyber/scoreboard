3151b5bfc156fd7df2a7b52667fa3026e36e6ab6
Foster McLane <fkmclane@gmail.com>
2018-10-07 17:45:02 -0400

    default to no db for mysql

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ services = {
     'FTP': {'proto': 'ftp', 'offset': 5, 'port': 21, 'file': 'DONOTDELETE', 'contents': 'asdf', 'dne': 'DOESNOTEXIST'},
     'SSH': {'proto': 'ssh', 'offset': 6, 'port': 22, 'username': 'asdf', 'password': 'asdf'},
     'HTTP': {'proto': 'http', 'offset': 7, 'port': 80, 'method': 'GET', 'url': '/', 'regex': 'asdf'},
-    'MySQL': {'proto': 'mysql', 'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf', 'db': 'asdf'},
+    'MySQL': {'proto': 'mysql', 'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf', 'db': ''},
 }
 
 interval = 60
