79ec48976c2b5b95c89dacccfa3b785d8518991d
Foster McLane <fkmclane@gmail.com>
2019-02-15 15:39:59 -0500

    use ordereddict for config

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-teams = {
-    'Team1': '10.0.130.0',
-    'Team2': '10.0.131.0',
-    'Team3': '10.0.132.0',
-    'Team4': '10.0.133.0',
-}
+import collections
 
-services = {
-    'FTP': {'proto': 'ftp', 'offset': 5, 'port': 21, 'file': 'DONOTDELETE', 'contents': 'asdf', 'dne': 'DOESNOTEXIST'},
-    'SSH': {'proto': 'ssh', 'offset': 6, 'port': 22, 'username': 'asdf', 'password': 'asdf'},
-    'HTTP': {'proto': 'http', 'offset': 7, 'port': 80, 'method': 'GET', 'url': '/', 'regex': 'asdf'},
-    'MySQL': {'proto': 'mysql', 'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf', 'db': ''},
-}
+teams = collections.OrderedDict()
+teams['Team1'] = '10.0.130.0',
+teams['Team2'] = '10.0.131.0',
+teams['Team3'] = '10.0.132.0',
+teams['Team4'] = '10.0.133.0',
+
+services = collections.OrderedDict()
+services['FTP'] = {'proto': 'ftp', 'offset': 5, 'port': 21, 'file': 'DONOTDELETE', 'contents': 'asdf', 'dne': 'DOESNOTEXIST'},
+services['SSH'] = {'proto': 'ssh', 'offset': 6, 'port': 22, 'username': 'asdf', 'password': 'asdf'},
+services['HTTP'] = {'proto': 'http', 'offset': 7, 'port': 80, 'method': 'GET', 'url': '/', 'regex': 'asdf'},
+services['MySQL'] = {'proto': 'mysql', 'offset': 8, 'port': 3306, 'username': 'asdf', 'password': 'asdf', 'db': ''},
 
 interval = 60
 timeout = 1
