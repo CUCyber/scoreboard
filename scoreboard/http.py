92696574138d60d5ed63482bdf77367f97fd297b
Foster McLane <fkmclane@gmail.com>
2019-03-06 16:26:07 -0500

    attempt to fix decode issue

--- scoreboard/http.py
+++ scoreboard/http.py
@@ -35,8 +35,11 @@ def check(addr, port, cert=None, method=None, headers=None, host=None, url=None,
             httpc.request(method, url, body, headers)
 
             if regex is not None:
-                up = up and re.search(regex, httpc.getresponse().read().decode())
-    except (http.client.HTTPException, socket.error, AttributeError):
+                if isinstance(regex, bytes):
+                    up = up and re.search(regex, httpc.getresponse().read())
+                else:
+                    up = up and re.search(regex, httpc.getresponse().read().decode())
+    except (http.client.HTTPException, socket.error, AttributeError, UnicodeDecodeError):
         up = False
     finally:
         try:
